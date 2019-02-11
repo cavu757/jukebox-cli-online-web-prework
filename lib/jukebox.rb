686c00e285ec929cb638d11e71735c5e013aea3c
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-02-11 05:34:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ def play(songs)
   
   puts "Please enter a song name or number:"
   user_input = gets.chomp
-  
+  p hsongs.keys
   if hsongs.values.include? user_input
     puts "Playing #{user_input}"
   elsif hsongs.keys.include? user_input
